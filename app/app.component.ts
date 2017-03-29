7232c828a89a0a7b2a1c48db77e1e86b472586f4
Caitlin Hines and Kaz Matthews <caitlin+kaz@>
2017-03-29 08:50:15 -0700

    list out keg information on homepage.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,9 +4,22 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <h1>Tap Room</h1>
+  <ul>
+    <li *ngFor="let keg of kegs">{{keg.name}}, {{keg.alcoholContent}} from {{keg.brand}} - {{keg.price}}</li>
+  </ul>
   `
 })
 
 export class AppComponent {
-  
+  kegs: Keg[] = [
+    new Keg('Spacedust', 'Elysian', 6, '5.9'),
+    new Keg('Spacedust', 'Elysian', 6, '5.9'),
+    new Keg('Spacedust', 'Elysian', 6, '5.9'),
+    new Keg('Spacedust', 'Elysian', 6, '5.9'),
+    new Keg('Spacedust', 'Elysian', 6, '5.9')
+  ]
+}
+
+export class Keg {
+  constructor(public name: string, public brand: string, public price: number, public alcoholContent: string) { }
 }
