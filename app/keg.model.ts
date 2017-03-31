456ae0ab202eac3bb243dcbc4be5dcc68824acc7
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-03-31 08:22:33 -0700

    adapt cloned version of previous angular project to beginning state.

--- app/keg.model.ts
+++
@@ -1,5 +0,0 @@
-export class Keg {
-  public pints: number = 124;
-  public happyHour: string = "put on happy hour";
-  constructor(public name: string, public brand: string, public style: string, public price: number, public alcoholContent: string) { }
-}
